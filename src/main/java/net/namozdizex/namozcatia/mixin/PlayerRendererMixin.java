2058773b1469e0037f1be56397ec46a617983c26
lpkll <lpkll123@gmail.com>
2022-10-14 23:22:36 +0700

    update glowing part armor

--- src/main/java/net/namozdizex/namozcatia/mixin/PlayerRendererMixin.java
+++ src/main/java/net/namozdizex/namozcatia/mixin/PlayerRendererMixin.java
@@ -11,7 +11,7 @@ import net.minecraft.client.renderer.entity.LivingEntityRenderer;
 import net.minecraft.client.renderer.entity.player.PlayerRenderer;
 import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.resources.ResourceLocation;
-import net.namozdizex.namozcatia.renderer.GlowingSkinLayer;
+import net.namozdizex.namozcatia.renderer.GlowingNamoZLayer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -22,18 +22,20 @@ public abstract class PlayerRendererMixin extends LivingEntityRenderer<AbstractC
     public PlayerRendererMixin(EntityRendererProvider.Context context, PlayerModel<AbstractClientPlayer> entityModel, float f) {
         super(context, entityModel, f);
     }
+
     @Inject(method = "<init>", at = @At("TAIL"))
     public void init(EntityRendererProvider.Context context, boolean bl, CallbackInfo ci) {
-        this.addLayer(new GlowingSkinLayer<>((PlayerRenderer) (Object) this));
+        this.addLayer(new GlowingNamoZLayer((PlayerRenderer) (Object) this));
     }
+
     @Inject(method = "renderHand", at = @At("TAIL"))
     private void renderArm(PoseStack poseStack, MultiBufferSource multiBufferSource, int i, AbstractClientPlayer abstractClientPlayer, ModelPart modelPart, ModelPart modelPart2, CallbackInfo ci) {
         float time = (float) abstractClientPlayer.tickCount;
         if (!abstractClientPlayer.isInvisible() && abstractClientPlayer.getName().getString().equals("NamoZDizeX")) {
             modelPart.xRot = 0.0f;
-            modelPart.render(poseStack, multiBufferSource.getBuffer(RenderType.eyes(new ResourceLocation("namozcatia", "textures/entity/special-skin/herrscher_of_the_summer.png"))), i, OverlayTexture.NO_OVERLAY, GlowingSkinLayer.makeFade(time), GlowingSkinLayer.makeFade(time), GlowingSkinLayer.makeFade(time), 1.0F);
+            modelPart.render(poseStack, multiBufferSource.getBuffer(RenderType.eyes(new ResourceLocation("namozutils", "textures/entity/special-skin/herrscher_of_the_summer.png"))), i, OverlayTexture.NO_OVERLAY, GlowingNamoZLayer.makeFade(time), GlowingNamoZLayer.makeFade(time), GlowingNamoZLayer.makeFade(time), 1.0F);
             modelPart2.xRot = 0.0f;
-            modelPart2.render(poseStack, multiBufferSource.getBuffer(RenderType.dragonExplosionAlpha(new ResourceLocation("namozcatia", "textures/entity/special-skin/herrscher_of_the_summer.png"))), i, OverlayTexture.NO_OVERLAY, GlowingSkinLayer.makeFade(time), GlowingSkinLayer.makeFade(time), GlowingSkinLayer.makeFade(time), 1.0F);
+            modelPart2.render(poseStack, multiBufferSource.getBuffer(RenderType.dragonExplosionAlpha(new ResourceLocation("namozutils", "textures/entity/special-skin/herrscher_of_the_summer.png"))), i, OverlayTexture.NO_OVERLAY, GlowingNamoZLayer.makeFade(time), GlowingNamoZLayer.makeFade(time), GlowingNamoZLayer.makeFade(time), 1.0F);
         }
     }
 }
--- src/main/java/net/namozdizex/namozcatia/renderer/GlowingNamoZLayer.java
+++ src/main/java/net/namozdizex/namozcatia/renderer/GlowingNamoZLayer.java
@@ -5,7 +5,8 @@ import com.mojang.blaze3d.vertex.VertexConsumer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.model.EntityModel;
+import net.minecraft.client.model.PlayerModel;
+import net.minecraft.client.player.AbstractClientPlayer;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.RenderLayerParent;
@@ -13,22 +14,24 @@ import net.minecraft.client.renderer.entity.layers.RenderLayer;
 import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.Mth;
-import net.minecraft.world.entity.Entity;
 
 @Environment(EnvType.CLIENT)
-public class GlowingSkinLayer<T extends Entity, M extends EntityModel<T>> extends RenderLayer<T, M> {
-    public GlowingSkinLayer(RenderLayerParent<T, M> renderLayerParent) {
+public class GlowingNamoZLayer extends RenderLayer<AbstractClientPlayer, PlayerModel<AbstractClientPlayer>>
+{
+
+    public GlowingNamoZLayer(RenderLayerParent<AbstractClientPlayer, PlayerModel<AbstractClientPlayer>> renderLayerParent) {
         super(renderLayerParent);
     }
+
     public static float makeFade(float alpha) {
         return Math.min(0.7F, (Mth.sin(alpha / 24) + 1F) / 2F + 0.15F);
     }
 
     @Override
-    public void render(PoseStack poseStack, MultiBufferSource multiBufferSource, int i, T entity, float f, float g, float h, float j, float k, float l) {
+    public void render(PoseStack poseStack, MultiBufferSource multiBufferSource, int i, AbstractClientPlayer entity, float f, float g, float h, float j, float k, float l) {
         float time = Minecraft.getInstance().getDeltaFrameTime() + entity.tickCount;
         if (entity.getName().getString().equals("NamoZDizeX")) {
-            VertexConsumer inveterate = multiBufferSource.getBuffer(RenderType.eyes(new ResourceLocation("namozcatia", "textures/entity/special-skin/herrscher_of_the_summer.png")));
+            VertexConsumer inveterate = multiBufferSource.getBuffer(RenderType.eyes(new ResourceLocation("namozutils", "textures/entity/special-skin/herrscher_of_the_summer.png")));
             this.getParentModel().renderToBuffer(poseStack, inveterate, i, OverlayTexture.NO_OVERLAY, makeFade(time), makeFade(time), makeFade(time), 1.0F);
         }
     }
